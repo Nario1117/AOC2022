5aa67ab256823faaebcd83a2ca7bcc6f98fb7604
Nario1117 <forgacnario2@gmail.com>
2022-12-07 22:23:10 +0100

    Day1 part2

--- src/Day01.java
+++ src/Day01.java
@@ -39,6 +39,52 @@ public class Day01 extends Day {
 
 	@Override
 	public String runPartTwo() throws IOException {
-		return null;
+		String input = readInput("day01_test.txt");
+		String[] elves = input.split("\r\n\r\n");
+		ArrayList<String[]> foodDoses = new ArrayList<>();
+		HashMap<Integer, Integer> mainFood = new HashMap();
+
+		for (int i = 0; i < elves.length; i++) {
+			foodDoses.add(elves[i].split("\r\n"));
+		}
+		int finalFood;
+		for (int y = 0; y < foodDoses.size(); y++) {
+			finalFood = 0;
+			for (int i = 0; i < foodDoses.get(y).length; i++) {
+				int food = Integer.parseInt(foodDoses.get(y)[i]);
+				finalFood += food;
+			}
+			mainFood.put(y, finalFood);
+		}
+		//for (int i = 0; i < mainFood.size(); i++) {
+		//	System.out.println(mainFood.get(i));
+		//}
+		int sizeCheck = 0;
+		int sizeCheck1 = 0;
+		int sizeCheck2 = 0;
+		int controlSmall;
+		int controlBig;
+		for (int i = 0; i < mainFood.size(); i++) {
+			if (mainFood.get(i) > sizeCheck) {
+				sizeCheck = mainFood.get(i);
+			}
+			if (sizeCheck > sizeCheck1) {
+				controlSmall = sizeCheck1;
+				controlBig = sizeCheck;
+				sizeCheck1 = controlBig;
+				sizeCheck = controlSmall;
+
+			}
+			if (sizeCheck1 > sizeCheck2) {
+				controlSmall = sizeCheck2;
+				controlBig = sizeCheck1;
+				sizeCheck1 = controlSmall;
+				sizeCheck2 = controlBig;
+
+			}
+		}
+		int bigOne = sizeCheck1 + sizeCheck2 + sizeCheck;
+
+		return Integer.toString(bigOne);
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Day day = new Day01();
-		System.out.println(day.runPartOne());
+		//System.out.println(day.runPartOne());
+		System.out.println(day.runPartTwo());
 	}
 }
